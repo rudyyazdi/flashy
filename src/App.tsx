26a03b00ed8beb8e25fd86c324c61fa20578e087
rudyyazdi <rudyyazdi@gmail.com>
2019-04-16 19:09:32 +1000

    tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
-        hi
+         hi
       </div>
     );
   }
