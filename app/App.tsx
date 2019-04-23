6e20ed1744512da353327658a5848606785ea45a
rudyyazdi <rudyyazdi@gmail.com>
2019-04-23 19:33:52 +1000

    stye ponits

--- app/App.tsx
+++ app/App.tsx
@@ -4,10 +4,25 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 import "./App.css";
 import Product from "./Product";
 
+const navStyle = {
+  display: "flex",
+  height: "3em",
+  alignItems: "center",
+  padding: "1em",
+  boxShadow: "1px 1px 5px grey"
+}
+
+const navLinkStyle = {
+  margin: "5px"
+}
+
 const App = () => (<div className="App">
   <BrowserRouter>
-    <Link to="/products">products</Link>
-    <Route exact path="/products" component={Product} />
+    <div style={navStyle} >
+      <Link to="/" style={navLinkStyle}>home</Link>
+      <Link to="/products" style={navLinkStyle}>products</Link>
+    </div>
+    <Route exact={true} path="/products" component={Product} />
   </BrowserRouter>
 </div>)
 
