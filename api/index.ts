47bd38401104a4a55cdcc23619bdf70f0ce39ec2
rudyyazdi <rudyyazdi@gmail.com>
2019-04-24 17:54:43 +1000

    fetch the products

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,22 @@
 import express from "express";
 
+import "../config/env"
+import { Product } from "./models"
+
 const app = express();
 const port: number = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
   res.send("Hello world!");
+});
+
+app.get("/products", (req, res) => {
+  Product
+    .find()
+    .then((products) => {
+      res.send({ data: products });
+    });
 });
 
 // start the Express server
--- app/Product/Product.tsx
+++ app/Product/Product.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Product = () => (
-  <div>hi :)</div>
-)
+const Product = () => {
+  const [products, setProducts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    setIsFetching(true);
+    fetch('products')
+      .then(res => res.json())
+      .then((res) => {
+        setProducts(res.data);
+        setIsFetching(false);
+      })
+      .catch(err => setIsFetching(false));
+  }, []);
+
+  return (
+    <div>
+      {products.map((product, i) => <div key={i} >product.name</div>)}
+    </div>
+  );
+};
 
 export default Product;
